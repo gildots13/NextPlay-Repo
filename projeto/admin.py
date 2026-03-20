173bf13abaf5e74cd877f1ecfec19a0df636c2e9
giltots13 <gildoworks12@gmail.com>
2026-03-20 16:29:02 -0300

    Django já administrando as tabelas e os dados

--- projeto/admin.py
+++ projeto/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Title
 
-# Register your models here.
+# Admin prévio
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'type', 'diretor', 'criador', 'ano', 'temporadas')
+    list_filter = ('type',)  # filtro por tipo no admin
+    search_fields = ('titulo', 'diretor', 'criador')
--- projeto/views.py
+++ projeto/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Title
 
-# Create your views here.
+def lista_filmes(request):
+    filmes = Title.objects.filmes()  # só filmes pelo type
+    return render(request, 'filmes/lista_filmes.html', {'filmes': filmes})
+
+def lista_series(request):
+    series = Title.objects.series()  # só series pelo type
+    return render(request, 'filmes/lista_series.html', {'series': series})
